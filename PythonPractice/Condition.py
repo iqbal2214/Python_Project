474f321aca6d0c7656adde91d1ae5794c94d31cd
iqbal2214 <iqbalrimtian2214@gmail.com>
2023-03-22 01:46:42 +0530

    PythonProject

---
+++ PythonPractice/Condition.py
@@ -0,0 +1,34 @@
+#if
+x = 10
+
+if x > 5:
+    print("x is greater than 5")
+
+#if else
+x = 3
+
+if x % 2 == 0:
+    print("x is even")
+else:
+    print("x is odd")
+
+#if elif else
+x = 10
+
+if x > 10:
+    print("x is greater than 10")
+elif x == 10:
+    print("x is equal to 10")
+else:
+    print("x is less than 10")
+
+#nested if else
+num = 12
+
+if num > 10:
+    if num % 2 == 0:
+        print("Number is greater than 10 and even.")
+    else:
+        print("Number is greater than 10 and odd.")
+else:
+    print("Number is less than or equal to 10.")
---
+++ PythonPractice/Table.py
@@ -0,0 +1,4 @@
+table=20
+
+for i in range(1,10):
+    print(table, "x", i, "=", table*i)
